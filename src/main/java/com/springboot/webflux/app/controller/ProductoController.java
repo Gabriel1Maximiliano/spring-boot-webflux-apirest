a0c285bd3674a5d92ae8cd99c12f268509b78f5f
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-11-18 09:51:39 -0300

    se agrega post con validaciones corregir error

--- src/main/java/com/springboot/webflux/app/controller/ProductoController.java
+++ src/main/java/com/springboot/webflux/app/controller/ProductoController.java
@@ -1,16 +1,19 @@
 package com.springboot.webflux.app.controller;
 
 import java.io.File;
-import java.lang.annotation.Retention;
+
 import java.net.URI;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
-import javax.annotation.PostConstruct;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,17 +23,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
 import com.springboot.webflux.app.models.document.Producto;
 import com.springboot.webflux.app.models.service.ProductoService;
 
+import io.netty.channel.ServerChannel;
+
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+import javax.validation.Valid;
 
 
 @RestController
@@ -54,6 +60,18 @@ public Mono<ResponseEntity<Producto>> upload(@PathVariable String id, @RequestPa
 	.defaultIfEmpty(ResponseEntity.notFound().build());
 
 }
+@PostMapping("/v2")// sube el producto con la foto
+public Mono<ResponseEntity<Producto>> crearConFoto( Producto producto, @RequestPart FilePart file) {
+	if(producto.getCreateAt()==null){
+		producto.setCreateAt(new Date());
+	}
+	producto.setFoto(UUID.randomUUID().toString()+"-"+file.filename().replace(" ",""));
+
+	return file.transferTo(new File(path + producto.getFoto())).then(service.save(producto))
+    .map(p-> ResponseEntity
+	.created(URI.create("/api/productos/".concat(p.getId()))).body(p));
+
+}
 	@GetMapping
 	public Mono<ResponseEntity<Flux<Producto>>>lista(){
 		
@@ -74,16 +92,54 @@ public Mono<ResponseEntity<Producto>> ver(
 }
 
 @PostMapping
-public Mono<ResponseEntity<Producto>> crear(@RequestBody Producto producto) {
-	if(producto.getCreateAt()==null){
-		producto.setCreateAt(new Date());
+public Mono<ResponseEntity<Producto>> crear(@RequestBody Producto producto){
+	if(producto.getCreateAt()==null) {
+ 		producto.setCreateAt(new Date());
 	}
 
-	return service.save(producto).map(p->ResponseEntity
+	return service.save(producto).map(p-> ResponseEntity
 	.created(URI.create("/api/productos/".concat(p.getId())))
-	.body(p) );
-
+	.body(p)
+	);
 }
+
+// @PostMapping
+// public Mono<ResponseEntity<Map<String, Object>>> crear(@Valid @RequestBody Mono<Producto> monoProducto){
+// 	Map<String, Object> respuesta = new HashMap<String, Object>();
+
+// 	return monoProducto.flatMap(producto -> {
+// 		if(producto.getCreateAt()==null) {
+// 			producto.setCreateAt(new Date());
+// 		}
+
+// 		return service.save(producto).map(p-> {
+// 			respuesta.put("producto", p);
+// 			respuesta.put("mensaje", "Producto creado con éxito");
+// 			respuesta.put("timestamp", new Date());
+// 			return ResponseEntity
+// 					.created(URI.create("/api/productos/".concat(p.getId())))
+// 					.body(respuesta);
+// 		});
+
+// 	}).onErrorResume(t -> {
+// 		return Mono.just(t).cast(WebExchangeBindException.class)
+// 				.flatMap(e -> Mono.just(e.getFieldErrors()))
+// 				.flatMapMany(Flux::fromIterable)
+// 				.map(fieldError -> "El campo "+fieldError.getField() + " " + fieldError.getDefaultMessage())
+// 				.collectList()
+// 				.flatMap(list -> {
+// 					respuesta.put("errors", list);
+// 					respuesta.put("timestamp", new Date());
+// 					respuesta.put("status", HttpStatus.BAD_REQUEST.value());
+// 					return Mono.just(ResponseEntity.badRequest().body(respuesta));
+// 				});
+
+// 	});
+
+
+// }
+
+
 
 @PutMapping("/{id}")
 public Mono<ResponseEntity<Producto>> editar(@RequestBody Producto producto, @PathVariable String id){
